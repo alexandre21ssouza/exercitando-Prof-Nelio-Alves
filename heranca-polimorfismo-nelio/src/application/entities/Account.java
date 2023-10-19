1aeac9587146434a9c2c0f383b36b7882b8bcb90
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2023-10-18 22:23:08 -0300

    Estruturação do projeto e Trabalhando com Herança

---
+++ heranca-polimorfismo-nelio/src/application/entities/Account.java
@@ -0,0 +1,77 @@
+package application.entities;
+
+/* Herança é uma associação entre classes
+ * (e não entre objetos), sendo assim, ao
+ * extender ou extends uma classe, elas se 
+ * tornam um só objeto e não dois distintos
+ * como na composição
+ */
+
+//Super classe ou classe base
+public class Account {
+	
+	
+	private Integer number;
+	private String holder;
+	
+	/*Para que o método (loan da classe BusinessAcount tenha acesso
+	ao balance) uso o protected, que pode ser acessado por outra classe
+	do mesmo pacote*/
+	protected Double balance;
+	
+	
+	public Account() {
+	}
+
+	
+	public Account(Integer number, String holder, Double balance) {
+		super();
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+
+	public Integer getnumber() {
+		return number;
+	}
+
+
+	public void setnumber(Integer number) {
+		this.number = number;
+	}
+
+
+	public String getHolder() {
+		return holder;
+	}
+
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+
+	public Double getBalance() {
+		return balance;
+	}
+
+
+	/*O set é removido pelo fato de não podermudar
+	 *  o saldo da conta livremente
+	 */
+//	public void setBalance(Double balance) {
+//		this.balance = balance;
+//	}
+	
+	
+	//Saque *balance é saldo
+	public void whithdraw(double amount) {
+		balance -= amount;
+	}
+	
+	//Depósito
+	public void deposit(double amount) {
+		balance += amount;
+	}
+}
---
+++ heranca-polimorfismo-nelio/src/application/entities/BusinessAccount.java
@@ -0,0 +1,45 @@
+package application.entities;
+
+//Classe derivada ou subclasse
+public class BusinessAccount extends Account{
+
+	private Double loanLimit;
+
+	public BusinessAccount() {
+		super();
+	}
+
+	
+	/*Construtor com parâmetros com atributos do Account dentro do super(que
+	 * serve para executar a lógica do construtor da super classe na 
+	 * subclasse ou classe derivada, devido esta classe extends a
+	 *  classe Account ou super classe*/
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+	
+	//Emprestar dinheiro
+	public void loan(double amount) {
+		
+		/*Lógica de Só empresta se o limite for 
+		  maior que o pedido de empréstimo */
+		if(amount <= loanLimit) {
+			balance +=amount - 10.0;
+		}
+		
+	}
+	
+
+	
+}
